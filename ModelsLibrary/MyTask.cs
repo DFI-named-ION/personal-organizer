149a986ae7e1ff8bec41456ecbf9dd4410d1c9ab
DFI-named-ION <larityandselnter@gmail.com>
2022-12-02 11:46:09 +0200

    Base classes complete

--- ModelsLibrary/MyTask.cs
+++ ModelsLibrary/MyTask.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace ModelsLibrary
 {
-    class MyTask
+    public class MyTask
     {
-        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public Category Category { get; set; }
     }
 }
